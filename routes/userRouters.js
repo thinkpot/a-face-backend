247b0375f7889573fb02b6ed7a5b73725e63f526
thinkpot <sk9668660@gmail.com>
2024-10-04 23:25:31 +0530

    change training steps to old

--- routes/userRouters.js
+++ routes/userRouters.js
@@ -32,8 +32,6 @@ router.get('/get-credits', async (req, res) => {
       if (!user) {
         return res.status(404).json({ message: 'User not found' });
       }
-
-      console.log(user)
   
       res.status(200).json({ credits: user.credits }); // Return the user's credits
     } catch (error) {
